e323e2611271be62c8941a30fc3ea37a427f2c7a
SiyaHarry <168104363+SiyaHarry@users.noreply.github.com>
2026-06-16 16:42:36 +0100

    Create Portfolio.java

---
+++ Portfolio.java
@@ -0,0 +1,30 @@
+package com.example.demo.entities;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Portfolio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long portfolioId;
+
+    @OneToOne
+    @JoinColumn(name = "client_id")
+    private Client client;
+
+    @OneToMany(mappedBy = "portfolio")
+    private List<Security> securities;
+
+    public Portfolio(Long portfolioId, Client client,
+                     List<Security> securities) {
+        this.portfolioId = portfolioId;
+        this.client = client;
+        this.securities = securities;
+    }
+
+    public Portfolio() {}
+
+    // Getters and Setters
+}
